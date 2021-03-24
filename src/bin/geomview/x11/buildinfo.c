fa7419629b82a40c57dfe3b26260de23638106c6
harvey <449540929@qq.com>
2021-03-24 18:17:32 +0800

    2021/03/24 18:17

--- src/bin/geomview/x11/buildinfo.c
+++ src/bin/geomview/x11/buildinfo.c
@@ -1,3 +1,3 @@
-char builddate[] = "202103151640";
-char buildinfo1[] = "  By hndx@hndx-computer[Linux-5.4.0-66-generic]";
-char buildinfo2[] = "  On 2021年 03月 15日 星期一 16:40:40 CST";
+char builddate[] = "202103171819";
+char buildinfo1[] = "  By hndx@hndx-computer[Linux-5.4.0-67-generic]";
+char buildinfo2[] = "  On 2021年 03月 17日 星期三 18:19:03 CST";
